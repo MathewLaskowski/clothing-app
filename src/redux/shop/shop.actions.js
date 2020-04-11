ec9e995d5fb34c247dee36bb2157a3dcaec4cf69
mlaskowski <maciejlaskowsky@gmail.com>
2020-04-11 11:22:44 +0200

    convert async actions from redux-thunk to redux-saga

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,5 +1,4 @@
 import ShopActionTypes from './shop.types';
-import { convertCollectionsSnapshotToMap, firestore } from '../../firebase/firebase.utils';
 
 export const fetchCollectionsStart = () => ({
   type: ShopActionTypes.FETCH_COLLECTIONS_START
@@ -14,15 +13,3 @@ export const fetchCollectionsFailure = errorMessage => ({
   type: ShopActionTypes.FETCH_COLLECTIONS_FAILURE,
   payload: errorMessage
 });
-
-export const fetchCollectionsStartAsync = () => {
-  return dispatch => {
-    const collectionRef = firestore.collection('collections');
-    dispatch(fetchCollectionsStart());
-
-    collectionRef.get().then(snapshot => {
-      const collectionMap = convertCollectionsSnapshotToMap(snapshot);
-      dispatch(fetchCollectionsSuccess(collectionMap));
-    }).catch(error => dispatch(fetchCollectionsFailure(error.message)))
-  }
-};
